dc2acdda54a532bf089856a9e9aa94fd064292f9
SungHwan Kim <kim20s2@hanmail.net>
2025-02-15 04:21:47 +0900

    4-1 확인문제 3번

---
+++ Chapter 4/4-1 확인문제 3번.py
@@ -0,0 +1,10 @@
+numbers = [273, 103, 5, 32, 65, 9, 72, 800, 99]
+
+for number in numbers:
+    if number % 2 == 1:
+        print(number, "는 홀수입니다.")
+    else:
+        print(number, "는 짝수입니다.")
+
+for number in numbers:
+    print(number, "는", len(str(number)), "자릿수입니다.")
